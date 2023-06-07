d5095c29135d2a2fae38ef8b1bb77e0fc7c6e472
Ken Sheridan <kensheridan86@gmail.com>
2023-06-07 11:43:53 +0100

    week-3-lab, number counter complete

--- Week-3-Labs/week-3-lab/src/App.js
+++ Week-3-Labs/week-3-lab/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-import TodoList from './components/TodoList';
+import React, { useState } from 'react';
+
 
 function App() {
-  return(
-    <div>
-      <TodoList />
+  const [number, setNumber] = useState(0);
+
+  const incrementNumber = () => {
+    setNumber(number + 1);
+  };
+
+  const decrementNumber = () => {
+    setNumber(number - 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Number: {number}</h1>
+      <button className="BtnSpace" onClick={decrementNumber}>Decrement</button>
+      <button className="BtnSpace" onClick={incrementNumber}>Increment</button>
+      
     </div>
-  )
+  );
 }
 
 export default App;
