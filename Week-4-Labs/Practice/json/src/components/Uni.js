e03398413892f3f012c1b8a5c20fb77e8d005fe9
Ken Sheridan <kensheridan86@gmail.com>
2023-06-13 12:09:39 +0100

    functionality to search universities by country complete

--- Week-4-Labs/Practice/json/src/components/Uni.js
+++ Week-4-Labs/Practice/json/src/components/Uni.js
@@ -1,11 +1,40 @@
 import { useState } from "react";
 
 function Uni(props) {
-    return (
-        <div>
-            Hello
-        </div>
-    )
+    const [moreInfo, setMoreInfo] = useState(false);
+
+    function handleShowMore(event) {
+        event.preventDefault();
+        setMoreInfo(true);
+    }
+
+    function handleShowLess(event) {
+        event.preventDefault();
+        setMoreInfo(false);
+    }
+
+    if(moreInfo) {
+        return (
+            <div>
+                {props.name}
+                <ul>
+                    <li>{props.country}</li>
+                    <li><a href={props.webPage} target="_blank" rel="noreferrer">{props.webPage}</a></li>
+                </ul>
+                <button onClick={handleShowLess}>Less Info</button>
+                <hr></hr>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                {props.name}
+                <br></br>
+                <button onClick={handleShowMore}>More Info</button>
+                <hr></hr>
+            </div>
+        )
+    }
 }
 
 export default Uni;
--- Week-4-Labs/Practice/json/src/components/Unis.js
+++ Week-4-Labs/Practice/json/src/components/Unis.js
@@ -3,9 +3,40 @@ import axios from "axios";
 import Uni from "./Uni";
 
 function Unis() {
+    const [unis, setUnis] = useState([]);
+    const [query, setQuery] = useState("");
+
+    function handleSearchQuery(event) {
+        event.preventDefault();
+        setQuery(event.target.value);
+    }
+
+    async function searchUnis(event) {
+        event.preventDefault();
+        let response = await axios.get("http://universities.hipolabs.com/search", {params: {country: query}});
+        setUnis(response.data);
+    }
+
     return (
         <div>
-            <Uni />
+            <input value={query} type="text" onChange={handleSearchQuery} placeholder="Enter a country name..." />
+            <button onClick={searchUnis}>Search</button>
+            <ul>
+            {
+                unis.map((uni, index) => {
+                    return (
+                        <li key={index}>
+                            <Uni key={index} 
+                                name={uni.name} 
+                                country={uni.country} 
+                                webPage={uni.web_pages[0]} 
+                            />
+                        </li>
+                    )
+                })
+            }
+            </ul>
+            
         </div>
     )
 }
