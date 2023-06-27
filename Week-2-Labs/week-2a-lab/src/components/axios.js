5aff56343ba14319cb93a0b5cca5bcfeaeccaf53
Ken Sheridan <kensheridan86@gmail.com>
2023-06-27 16:16:05 +0100

    week 4 lab updated for week 5

--- Week-2-Labs/week-2a-lab/src/components/axios.js
+++ Week-2-Labs/week-2a-lab/src/components/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.get('https://api.example.com/data')
     .then(response => {
-        console.log(response.data);
+        console.log("I've been sent from axios!");
     })
         .catch(error => {
         console.error(error);
--- Week-4-Labs/open-weather-api/src/components/Weather.js
+++ Week-4-Labs/open-weather-api/src/components/Weather.js
@@ -1,10 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Weather = () => {
     const [city, setCity] = useState('');
     const [weatherData, setWeatherData] = useState('');
     const [error, setError] = useState('');
+    const [long, setLong] = useState(0);
+    const [lat, setLat] = useState(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!("geolocation" in navigator)) {
+                setError("Geolocation is either not supported by your browser or you have not granted permission to access it");
+            }
+            navigator.geolocation.getCurrentPosition(async function(position){
+            setLat(position.coords.latitude);
+            setLong(position.coords.longitude);
+            let findPosition = await axios("https://geocode.xyz/" + position.coords.latitude + "," + position.coords.longitude, {params: {geoit: 'json'}});
+            console.log(findPosition.data.city);
+            setCity(findPosition.data.city);
+            });
+        };
+        fetchData();
+    }, [lat, long]);
 
     const handleChange = (event) => {
         setCity(event.target.value);
@@ -47,8 +65,8 @@ const Weather = () => {
         );
     } else {
         return (
-
             <div>
+                <p>Your current coordinates are: Lat: {lat}, Long: {long}.<br></br> That places you in: {city}</p> 
                 <form onSubmit={handleSubmit}>
                 <input
                     type="text"
